48e8c689af533c522e535062a269886f33b9da4b
Marie20767 <marie20767@gmail.com>
2026-08-21 16:51:59 +1000

    Add Cloudflare full proxy terraform config

--- terraform/staging/cloudflare/dns.tf
+++ terraform/staging/cloudflare/dns.tf
@@ -6,3 +6,13 @@ resource "cloudflare_record" "root" {
   proxied = true
   ttl     = 1
 }
+
+resource "cloudflare_zone_settings_override" "settings" {
+  zone_id = var.cloudflare_zone_id
+  settings {
+    ssl                      = "full"
+    always_use_https         = "on"
+    min_tls_version          = "1.2"
+    automatic_https_rewrites = "on"
+  }
+}
